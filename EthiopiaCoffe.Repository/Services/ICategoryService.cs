055e7ee3f313462ddaaaa54137617ace3b28d3d5
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2024-05-10 01:02:52 +0300

    create service interfaces

---
+++ EthiopiaCoffe.Repository/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using EthiopiaCoffe.Repository.DTOs.Category;
+using EthiopiaCoffe.Repository.DTOs;
+using EthiopiaCoffe.Domain.Concrete.Entities;
+using EthiopiaCoffe.Repository.DTOs.Product;
+
+namespace EthiopiaCoffe.Repository.Services
+{
+    public interface ICategoryService:IGenericService<Category,CategoryDTO>
+    {
+        ResponseDTO<List<CategoryWithProductsDTO>> CategoryWithProducts();
+        Task<ResponseDTO<Guid>> AddAsync(CategoryAddDTO entity);
+        ResponseDTO<NoContent> Update(CategoryUpdateDTO entity);
+    }
+}
---
+++ EthiopiaCoffe.Repository/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using EthiopiaCoffe.Domain.Concrete.Entities;
+using EthiopiaCoffe.Repository.DTOs;
+using EthiopiaCoffe.Repository.DTOs.Product;
+
+namespace EthiopiaCoffe.Repository.Services
+{
+    public interface IProductService : IGenericService<Product, ProductDTO> 
+    {
+        ResponseDTO<List<ProductWithCategoryDTO>> ProductsWithCategory();
+        Task<ResponseDTO<Guid>> AddAsync(ProductAddDTO entity);
+        ResponseDTO<NoContent> Update(ProductUpdateDTO entity);
+    }
+}
