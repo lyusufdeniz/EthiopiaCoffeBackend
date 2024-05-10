22ffeb640eb1cb937c8d7f6fda7f14c81f5c630c
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2024-05-10 03:52:35 +0300

    add offer repo

---
+++ EthiopiaCoffe.Persistence/Repositories/OfferRepository.cs
@@ -0,0 +1,19 @@
+﻿using EthiopiaCoffe.Domain.Concrete.Entities;
+using EthiopiaCoffe.Repository.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EthiopiaCoffe.Persistence.Repositories
+{
+    public class OfferRepository : GenericRepository<Offer>, IOfferRepository
+    {
+        public OfferRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+
+        public IQueryable<Offer> OffersWithCategory()
+        {
+            return _context.Offers.Include(o => o.Category); 
+        }
+    }
+}
