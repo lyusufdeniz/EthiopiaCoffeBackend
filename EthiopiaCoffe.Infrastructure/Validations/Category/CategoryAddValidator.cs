55590e9cc3a0616de1842a7f9fac1a2d3960b921
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2024-05-13 18:41:44 +0300

    update validators

--- EthiopiaCoffe.Infrastructure/Validations/Category/CategoryAddValidator.cs
+++ EthiopiaCoffe.Infrastructure/Validations/Category/CategoryAddValidator.cs
@@ -7,9 +7,9 @@ namespace EthiopiaCoffe.Infrastructure.Validations.Category
     {
         public CategoryAddValidator()
         {
-            RuleFor(x => x.Name).Length(3, 15).WithMessage("{PropertyName} Length must between 3 and 15");
+            RuleFor(x => x.Name).Length(3, 30).WithMessage("{PropertyName} Length must between 3 and 15");
             RuleFor(x => x.Name).NotNull().NotEmpty().WithMessage("{PropertyName} must not be null");
-            RuleFor(x => x.Description).Length(3, 50).WithMessage("{PropertyName} Length must between 3 and 50");
+            RuleFor(x => x.Description).Length(3, 80).WithMessage("{PropertyName} Length must between 3 and 50");
             RuleFor(x => x.Description).NotNull().NotEmpty().WithMessage("{PropertyName} must not be null");
 
 
