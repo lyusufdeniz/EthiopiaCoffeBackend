8487e4f5cdc34b736652f1eeb4b5aa27f5861246
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2024-05-25 01:48:40 +0300

    add middleware extension

---
+++ EthiopiaCoffe.API/Extensions/MiddlewareInjectorExtension.cs
@@ -0,0 +1,36 @@
+﻿using EthiopiaCoffe.API.Middlewares;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace EthiopiaCoffe.API.Extensions
+{
+    public static class MiddlewareInjectorExtension
+    {
+        public static void InjectMiddlewares(this WebApplication app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (exceptionHandlerFeature != null)
+                    {
+                        var exception = exceptionHandlerFeature.Error;
+                        var handler = app.Services.GetRequiredService<GlobalExceptionHandler>();
+                        await handler.TryHandleAsync(context, exception, context.RequestAborted);
+                    }
+                });
+            });
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+        }
+
+    }
+}
