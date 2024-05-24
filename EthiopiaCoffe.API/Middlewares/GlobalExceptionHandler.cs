1720a9cc8f6bacabdffdeba8ec9d8e5ea23515d5
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2024-05-25 02:06:42 +0300

    add global exception handler

---
+++ EthiopiaCoffe.API/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,17 @@
+﻿using EthiopiaCoffe.Repository.DTOs;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace EthiopiaCoffe.API.Middlewares
+{
+    public class GlobalExceptionHandler:IExceptionHandler
+    {
+  //handle exception and write own response
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            var responseModel = ResponseDTO<NoContent>.Fail(exception.Message,System.Net.HttpStatusCode.InternalServerError);
+             await httpContext.Response.WriteAsJsonAsync(responseModel, cancellationToken: cancellationToken);
+            return true;
+        }
+    }
+}
